441af6e7638c95eb48e04891c671f4e71e9169be
pranavrbm <pranavbm05@gmail.com>
2025-10-27 16:26:44 +0000

    Completed user services

--- userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -1,12 +1,25 @@
 package com.fitness.userservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@AllArgsConstructor
 public class UserController {
+
+    private UserService userService;
     @GetMapping("/{userId}")
-    public 
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId){
+        return ResponseEntity.ok(userService.getUserProfile(userId));
+    }
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request ){
+        return ResponseEntity.ok(userService.register(request));
+    }
 }
