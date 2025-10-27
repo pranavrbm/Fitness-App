c6b868a3dae766722a78e066628e786f80e10335
pranavrbm <pranavbm05@gmail.com>
2025-10-27 00:57:47 +0000

    [init] initialised the project

---
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.fitness.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @GetMapping("/{userId}")
+    public 
+}
