e3dc4951e113c4af1ff19170f33bc478f62e286f
pranavRBM <pranavbm05@gmail.com>
2025-11-08 17:07:48 +0530

    completed the whole project with working frontend

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "react-oauth2-code-pkce";
 import { useDispatch } from "react-redux";
@@ -10,17 +10,28 @@ import {
 	useLocation,
 } from "react-router";
 import { setCredentials } from "./store/authSlice";
+import ActivityForm from "./components/ActivityForm";
+import ActivityList from "./components/ActivityList";
+import ActivityDetail from "./components/ActivityDetail";
+
+const ActvitiesPage = () => {
+	return (
+		<Box sx={{ p: 2, border: "1px dashed grey" }}>
+			<ActivityForm onActivitiesAdded={() => window.location.reload()} />
+			<ActivityList />
+		</Box>
+	);
+};
 
 function App() {
 	const { token, tokenData, logIn, logOut, isAuthenticated } =
 		useContext(AuthContext);
 	const dispatch = useDispatch();
 	const [authReady, setAuthReady] = useState(false);
+
 	useEffect(() => {
-		if (token && tokenData) {
-			dispatch(
-				setCredentials({ token, user: tokenData?.preferred_username })
-			);
+		if (token) {
+			dispatch(setCredentials({ token, user: tokenData }));
 			setAuthReady(true);
 		}
 	}, [token, tokenData, dispatch]);
@@ -28,19 +39,73 @@ function App() {
 	return (
 		<Router>
 			{!token ? (
-				<Button
-					variant="contained"
-					color="#dc004e"
-					onClick={() => {
-						logIn();
+				<Box
+					sx={{
+						height: "100vh",
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						justifyContent: "center",
+						textAlign: "center",
 					}}>
-					Hello World
-				</Button>
+					<Typography
+						variant="h4"
+						gutterBottom>
+						Welcome to the Fitness Tracker App
+					</Typography>
+					<Typography
+						variant="subtitle1"
+						sx={{ mb: 3 }}>
+						Please login to access your activities
+					</Typography>
+					<Button
+						variant="contained"
+						color="primary"
+						size="large"
+						onClick={() => {
+							logIn();
+						}}>
+						LOGIN
+					</Button>
+				</Box>
 			) : (
-				<div>
-					<pre>{JSON.stringify(tokenData, null, 2)}</pre>
-					<pre>{token}</pre>
-				</div>
+				// <div>
+				//   <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+				//   <pre>{JSON.stringify(token, null, 2)}</pre>
+				// </div>
+
+				<Box sx={{ p: 2, border: "1px dashed grey" }}>
+					<Button
+						variant="contained"
+						color="secondary"
+						onClick={logOut}>
+						Logout
+					</Button>
+					<Routes>
+						<Route
+							path="/activities"
+							element={<ActvitiesPage />}
+						/>
+						<Route
+							path="/activities/:id"
+							element={<ActivityDetail />}
+						/>
+
+						<Route
+							path="/"
+							element={
+								token ? (
+									<Navigate
+										to="/activities"
+										replace
+									/>
+								) : (
+									<div>Welcome! Please Login.</div>
+								)
+							}
+						/>
+					</Routes>
+				</Box>
 			)}
 		</Router>
 	);
